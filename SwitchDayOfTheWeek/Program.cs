6be0d1979290ca0ae447f2911dddfa933173726a
mart <mart.ako.paris@tktk.ee>
2024-09-20 13:21:56 +0300

    add week3ifandswitch code

---
+++ SwitchDayOfTheWeek/Program.cs
@@ -0,0 +1,33 @@
+﻿int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+switch(weekDay)
+{
+    case 0:
+        Console.WriteLine("It is Sunday!");
+        break;
+        case 1:
+        Console.WriteLine("It is Monday!");
+        break;
+        case 2:
+        Console.WriteLine("It is Tuesday!");
+        break;
+        case 3:
+        Console.WriteLine("It is Wednesday!");
+        break;
+        case 4:
+        Console.WriteLine("It is Thursday!");
+        break;
+        case 5:
+        Console.WriteLine("It is Friday!");
+        break;
+        case 6:
+        Console.WriteLine("It is Saturday!");
+        break;
+        default:
+        Console.WriteLine("Oops, your calander must be broken.");
+        break;
+
+
+}
+
+Console.WriteLine("Have a nice day!");
---
+++ Week3IfandSwitch/Program.cs
@@ -0,0 +1,9 @@
+﻿//moodulus
+
+int a = 24;
+int b = 8;
+
+int c = b % a;
+
+Console.WriteLine(c);
+
